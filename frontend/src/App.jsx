7236801e95736b68ad2447065a9ceddb3a99dc9f
Vedika-nchoudhary <vedikachoudhry31@gmail.com>
2026-05-19 23:29:00 +0530

    conflicts

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -31,7 +31,6 @@ function App() {
   return (
     <>
       <NavBar />
-
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/events" element={<EventPage />} />
